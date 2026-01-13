c9cb52ed15e251a760894f4a7271da39c93140e7
Thomas <thoma@example.com>
2026-01-13 22:20:03 +0100

    FIX: Force Redeploy - Admin-Adresse sollte jetzt funktionieren

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
-// Force redeploy - Environment Variable Update
+// Force redeploy - Admin Address Update v2
 // Services
 import * as delegateRegistry from './services/delegateRegistry.js';
 import * as pointsService from './services/pointsService.js';
